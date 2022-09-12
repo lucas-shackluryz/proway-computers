d17cbc9b534f4d952ad125a2a5fd3e08ea3a65cc
Shackluryz <lucas.shackluryz@gmail.com>
2022-09-11 21:35:12 -0300

    [ ADD ][ UPDATE ] Atualiza barra-pesquisa e implementa logica

--- proway-computers/src/app/app.module.ts
+++ proway-computers/src/app/app.module.ts
@@ -8,19 +8,23 @@ import { AppRoutingModule } from './app-routing.module';
 import { NaoEncontradoComponent } from './nao-encontrado/nao-encontrado.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatSnackBarModule } from "@angular/material/snack-bar";
+import { BarraPesquisaComponent } from './barra-pesquisa/barra-pesquisa.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    NaoEncontradoComponent
+    NaoEncontradoComponent,
+    BarraPesquisaComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatSnackBarModule
+    MatSnackBarModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
